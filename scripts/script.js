cf6bea1fd6bf2f97221908a56626b08918024ee8
Junier Bolivar <junierba@yahoo.co.uk>
2024-05-04 15:50:42 +0200

    finished exercise

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let myButton = document.getElementById('shareButton');
+let mainModal = document.getElementById('myModal');
+
+myButton.addEventListener('click', (e) => {
+  mainModal.style.opacity = '1';
+  setTimeout(() => {
+    mainModal.style.opacity = '0';
+  }, 5000);
+});
