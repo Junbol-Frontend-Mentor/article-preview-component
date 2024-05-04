e7d47140c77dab17618a00cefab443e0340b8ff0
Junier Bolivar <junierba@yahoo.co.uk>
2024-05-04 16:05:00 +0200

    add comment to js

--- scripts/script.js
+++ scripts/script.js
@@ -5,5 +5,5 @@ myButton.addEventListener('click', (e) => {
   mainModal.style.opacity = '1';
   setTimeout(() => {
     mainModal.style.opacity = '0';
-  }, 5000);
+  }, 5000); //after 5sec the modal is gone
 });
